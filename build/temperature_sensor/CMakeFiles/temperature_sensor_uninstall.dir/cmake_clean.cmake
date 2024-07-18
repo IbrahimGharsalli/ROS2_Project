977bb018a3f1283a0e3d6c6233d244268d508884
ibrahim <gharsalli.ibrahim@gmail.com>
2024-07-18 08:46:38 +0100

    add folders

--- build/temperature_sensor/CMakeFiles/temperature_sensor_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/temperature_sensor_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/temperature_sensor_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
