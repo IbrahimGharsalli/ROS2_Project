6c7f2d2cfdb95d83f2ffcc920ccbd5daa8cc23dc
ibrahim <gharsalli.ibrahim@gmail.com>
2024-07-24 20:24:25 +0100

    add folders

---
+++ build/data_processor/CMakeFiles/data_processor_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_processor_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/data_processor_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/plot_sensor_data/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/cyber/ros2_ws/install/plot_sensor_data'
